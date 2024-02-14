c8593996267431fb6de99550c2f3ec0dfbed72e9
Diego Alejandro Campos <dcamposm@unal.edu.co>
2024-02-13 23:36:07 -0500

    Update prueba.cpp

--- c++ overview/prueba.cpp
+++ c++ overview/prueba.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <math.h>
 
 
 bool prime(int n);
-const int number = 30;
+const int top = 30;
+const int bottom = 10;
 
 int main(){
-    for (int i = number; i > 1; i--){
+    for (int i = top; i >= bottom; i--){
         if (prime(i)) std::cout << i << std::endl;
     }
 } 
 
 bool prime(int n){
-    double raiz = std::sqrt(n);
-    for (int i = n - 1; i >= raiz; i--){
+    for (int i = n - 1; i*i >= n; i--){
         if (n%i == 0){
             return false;
         }
